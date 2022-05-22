d8f30a35d4784f649f3db99159674e45b8c58619
GitBerg <beguinhoboy@hotmail.com>
2022-05-22 20:17:21 -0300

    first commit frontend

---
+++ javascript_note/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Section, Notification } from 'rbx'
+import './App.scss'
+import Header from './components/header'
+
+const App = () => (
+  <>
+    <Header/>
+  </>
+)
+
+export default App;
