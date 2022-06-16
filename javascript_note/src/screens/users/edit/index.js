12a2a6a6daac185a87144d2cb204bf4f820d1a3e
GitBerg <beguinhoboy@hotmail.com>
2022-06-16 01:20:22 -0300

    update api and created the edit user screen

--- javascript_note/src/screens/users/edit/index.js
+++ javascript_note/src/screens/users/edit/index.js
@@ -1,5 +1,4 @@
-import React, { Fragment } from 'react';
-import { Column, Section, Title, Container, Card, Button } from "rbx";
+import { Column, Section, Title, Container, Card } from "rbx";
 import "../../../styles/users.scss";
 import HeaderLogged from "../../../components/header_logged";
 
